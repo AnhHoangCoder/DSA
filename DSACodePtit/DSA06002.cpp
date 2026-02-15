387b5616153bb17d7ca593164bd441e47d836b51
May <hoangquocanh2n6@gmail.com>
2026-02-15 22:59:51 +0700

    Sắp xếp theo giá trụ tuyệt đối

---
+++ DSACodePtit/DSA06002.cpp
@@ -0,0 +1,68 @@
+// Cho mảng A[] gồm n phần tử và số X. Hãy đưa sắp xếp các phần tử của mảng theo trị tuyệt đối của |X - A[i] |. Ví dụ với A[] = {10, 5, 3, 9, 2} và X = 7 ta đưa ra mảng được sắp xếp theo nguyên tắc kể trên: A[] = {5, 9, 10, 3, 2} vì |7-10|=3, |7-5|=2, |7-3|=4, |7-9|=2, |7-2|=5.
+
+// Input:
+
+// Dòng đầu tiên đưa vào số lượng bộ test T.
+// Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n và X; dòng tiếp theo là n số A [i] của mảng A [];các số được viết cách nhau một vài khoảng trống.
+// T, n, X thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, X, A[i] ≤105.
+// Output:
+
+// Đưa ra kết quả mỗi test theo từng dòng.
+// Input:
+
+// 2
+// 5 7
+// 10 5 3 9 2
+// 5 6
+// 1 2 3 4 5
+ 
+// Output:
+
+// 5 9 10 3 2
+// 5 4 3 2 1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int x;
+
+bool cmp(int a, int b){
+    int num1 = abs(x - a);
+    int num2 = abs(x - b);
+
+    if(num1 < num2) return true;
+    return false;
+}
+
+void testCase(){
+    int n;
+    cin >> n >> x;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    sort(a.begin() , a.end() , cmp);    
+    for(int i=0;i<n;i++){
+        cout << a[i];
+        if(i < n-1){
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        testCase();
+    }
+    return 0;
+}
