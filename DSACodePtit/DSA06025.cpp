e050e4dd5e8b9897fdf930e49a75a0c2b47f1ed4
May <hoangquocanh2n6@gmail.com>
2026-02-24 22:57:00 +0700

    Sắp xếp chèn

--- DSACodePtit/DSA06025.cpp
+++ DSACodePtit/DSA06025.cpp
@@ -35,11 +35,13 @@ void testCase(){
         cin >> a[i];
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(a[j] > a[i]){
-                swap(a[i] , a[j]);
-            }
+        int key = a[i];
+        int j = i-1;
+        while(j >= 0 && a[j] > key){
+            a[j+1] = a[j];
+            j--;
         }
+        a[j+1] = key;
         cout << "Buoc " << i << ": ";
         for(int k=0;k<=i;k++){
             cout << a[k];
