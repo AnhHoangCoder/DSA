e6a4be9c7f7b70080749ed873e860acd6b5016b9
May <hoangquocanh2n6@gmail.com>
2026-06-06 22:52:10 +0700

    Catalan number

--- DSACodePtit/DSA05014.cpp
+++ DSACodePtit/DSA05014.cpp
@@ -29,23 +29,61 @@
 #include <vector>
 
 using namespace std;
-using ll = long long;
+
+vector<int> add(vector<int> &a , vector<int> &b){
+    vector<int> res;
+    int carry = 0;
+    int n = max(a.size() , b.size());
+
+    for(int i = 0; i < n || carry; i++){
+        int x = carry;
+        if(i < a.size()) x += a[i];
+        if(i < b.size()) x += b[i];
+        
+        res.push_back(x % 10);
+        carry = x / 10;
+    }
+    return res;
+}
+
+vector<int> mul(vector<int> &a , vector<int> &b){
+    vector<int> res(a.size() + b.size() , 0);
+
+    for(int i = 0; i < a.size(); i++){
+        for(int j = 0; j < b.size(); j++){
+            res[i + j] += a[i] * b[j];
+        }
+    }
+
+    for(int i = 0; i < res.size(); i++){
+        if(res[i] >= 10){
+            if(i + 1 == res.size()) res.push_back(0);
+            res[i + 1] += res[i] / 10;
+            res[i] %= 10;
+        }
+    }
+
+    while(res.size() > 1 && res.back() == 0) res.pop_back();
+    return res;
+}
 
 void testCase(){
     int n;
     cin >> n;
-
-    vector<ll> dp(n+1 , 0);
-
-    dp[0] = 1;
-    for(int i=1;i<=n;i++){
-        dp[i] = 0;
-        for(int j=0;j<i;j++){
-            dp[i] += dp[j] * dp[i - 1 - j];
+    vector<vector<int>> dp(n + 1);
+    dp[0] = {1};
+    for(int i = 1; i <= n; i++){
+        dp[i].clear();
+        for(int j = 0; j < i; j++){
+            vector<int> tmp = mul(dp[j] , dp[i - j - 1]);
+            dp[i] = add(dp[i] , tmp);
         }
     }
 
-    cout << dp[n] << endl;
+    for(int i = dp[n].size() - 1; i >= 0; i--){
+        cout << dp[n][i];
+    }
+    cout << endl;
 }
 
 int main(){
