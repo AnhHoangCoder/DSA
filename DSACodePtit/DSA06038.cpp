030726fc00ba068f6099058e2c14d99e1221ea4e
May <hoangquocanh2n6@gmail.com>
2026-06-22 22:43:45 +0700

    Cặp số

---
+++ DSACodePtit/DSA06038.cpp
@@ -0,0 +1,26 @@
+// Cho dãy số nguyên A có n phần tử.
+
+// Hãy đếm xem có bao nhiêu cặp (i,j) thỏa mãn:
+
+// i < j
+// A[i] > A[j] và đều là số chẵn
+// Tồn tại chỉ số k với i < k < j sao cho A[k] là số lẻ
+// Input
+
+// Dòng đầu tiên ghi số n (1 ≤ n ≤ 105).
+
+// Output
+
+// Dòng thứ 2 ghi n số của dãy A, các giá trị A[i] không vượt quá 106.
+
+// Ví dụ
+
+// Input
+
+// Output
+
+// 5
+
+// 4 3 2 5 1
+
+// 1
