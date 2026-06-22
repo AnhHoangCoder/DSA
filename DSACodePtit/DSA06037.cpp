7a9097b6f7c558d7066bd4bf1469264f64d0a6fd
May <hoangquocanh2n6@gmail.com>
2026-06-22 22:43:27 +0700

    Sắp xếp đoạn con

---
+++ DSACodePtit/DSA06037.cpp
@@ -0,0 +1,36 @@
+// Cho dãy số nguyên A[] độ dài N. Xác định số lượng chỉ số M (M < N) thỏa mãn: nếu sắp xếp đoạn con (A1,…,AM) và (AM+1, …, AN) theo thứ tự tăng dần thì được dãy số A tăng dần.
+
+// Input
+
+// Dòng đầu tiên ghi số bộ test T
+// Mỗi bộ test bắt đầu bằng một dòng chứa số N (2 ≤ N ≤ 105)
+// Dòng tiếp theo ghi N số của dãy A (|Ai| ≤ 109)
+// Output
+
+// Với mỗi bộ test:
+
+// Dòng đầu tiên ghi số K là số lượng vị trí M tìm được
+// Dòng thứ 2 ghi ra K số theo thứ tự tăng dần lần lượt là các chỉ số thỏa mãn. Nếu K = 0 thì dòng này bỏ trống. 
+// Ví dụ
+
+// Input
+
+// Output
+
+// 2
+
+// 2
+
+// 2 1
+
+// 5
+
+// 2 1 3 5 4
+
+// 0
+
+ 
+
+// 2
+
+// 2 3
