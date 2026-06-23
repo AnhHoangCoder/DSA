44e9a7ad5cfa39dca97be1fba6fd04c04d0a3424
May <hoangquocanh2n6@gmail.com>
2026-06-23 22:06:02 +0700

    Mạng xã hội

---
+++ DSACodePtit/DSA09033.cpp
@@ -0,0 +1,55 @@
+// Tý đang xây dựng một mạng xã hội và mời các bạn của mình dùng thử. Bạn của bạn cũng là bạn. Vì vậy, Tý muốn mạng xã hội của mình là hoàn hảo, tức với mọi bộ ba X, Y, Z, nếu X kết bạn với Y, Y kết bạn với Z thì X và Z cũng phải là bạn bè của nhau trên mạng xã hội.
+
+// Các bạn hãy xác định xem mạng xã hội hiện tại của Tý có là hoàn hảo hay không? Nếu có hãy in ra “YES”, “NO” trong trường hợp ngược lại.
+
+// Input:
+
+// Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
+// Mỗi test bắt đầu bởi 2 số nguyên N và M (N, M ≤ 100 000).
+// M dòng tiếp theo, mỗi dòng gồm 2 số nguyên u, v (u #v) cho biết u và v là kết bạn với nhau trên mạng xã hội của Tý.
+// Output: 
+
+// Với mỗi test, in ra đáp án tìm được trên một dòng.
+// Ví dụ:
+
+// Input:
+
+// Output
+
+// 3
+
+// 4 3
+
+// 1 3
+
+// 3 4
+
+// 1 4
+
+// 4 4
+
+// 3 1
+
+// 2 3
+
+// 3 4
+
+// 1 2
+
+// 10 4
+
+// 4 3
+
+// 5 10
+
+// 8 9
+
+// 1 2
+
+ 
+
+// YES
+
+// NO
+
+// YES
