f312e8f81ce0b20dce089bba6d5be270fd7645d1
May <hoangquocanh2n6@gmail.com>
2026-06-24 21:59:54 +0700

    Xâu đối xứng 2

---
+++ DSACodePtit/DSA05033.cpp
@@ -0,0 +1,22 @@
+// Cho xâu ký tự S. Nhiệm vụ của bạn là tìm số phép loại bỏ ít nhất các ký tự trong S để S trở thành xâu đối xứng. Chú ý, phép loại bỏ phải bảo toàn tính trước sau của các ký tự trong S.
+
+// Input:
+
+// Dòng đầu tiên đưa vào số lượng bộ test T.
+// Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một xâu ký tự được viết trên một dòng
+// T, str thỏa mãn ràng buộc: 1≤T≤100;  1≤length(S)≤100.
+// Output:
+
+// Đưa ra kết quả mỗi test theo từng dòng.
+// Ví dụ:
+
+//  Input
+
+// Output
+
+// 2
+// aebcbda
+// geeksforgeeks
+
+// 2
+// 8
