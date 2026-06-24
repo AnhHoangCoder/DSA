5b696abb574f3a8f26d1796eb9a8633b41988b6f
May <hoangquocanh2n6@gmail.com>
2026-06-24 21:55:23 +0700

    Giá trị lớn nhất

---
+++ DSACodePtit/DSA03027.cpp
@@ -0,0 +1,29 @@
+// Cho dãy số nguyên A[] có N phần tử.
+
+// Gọi f(i,j) = |ai| + |ai+1| + … + |aj|
+// Goij g(i,j) = ai + ai+1 + … + aj
+// Với tất cả các cặp 1 ≤ i ≤ j ≤ N.
+
+// Hãy tính giá trị lớn nhất của f(i,j) + g(i,j).
+
+// Input
+
+// Dòng đầu ghi số N (1 ≤ N ≤ 50000)
+
+// Dòng thứ 2 ghi N số nguyên của dãy A[]
+
+// Output
+
+// Ghi ra giá trị lớn nhất của f(i,j) + g(i,j)
+
+// Ví dụ
+
+// Input
+
+// Output
+
+// 5
+
+// -3 5 -10 8 -2
+
+// 26
